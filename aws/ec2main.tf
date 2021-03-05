3da1a19128dd8037ccd86be4abf1659b0726ba2c
Dave McKinstry <dmckinstry@users.noreply.github.com>
2021-03-04 18:34:50 -0700

    Update and rename aws/ec2/ec2.tf to aws/ec2main.tf

---
+++ aws/ec2main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  profile = "default"
+  region = "us-east-1"
+}
+resource "aws_instance" "test-sm-ec2" {
+  ami = "ami-08f3d892de259504d" #make sure to get the updated ami name for your region
+  instance_type = "t2.micro" #choose which size to use, be aware of costs if going out of free tier
+}
