6528e9aec5e342fd436222ee915885d5df3a4c55
Dave McKinstry <dmckinstry@users.noreply.github.com>
2021-03-06 15:56:51 -0700

    Update main.tf

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   profile = "default"
-  region = "us-east-1"
+  region = "us-west-2"
 }
 resource "aws_instance" "test-sm-ec2" {
-  ami = "ami-08f3d892de259504d" #make sure to get the updated ami name for your region
+  ami = "ami-b9401b89" #make sure to get the updated ami name for your region
   instance_type = "t2.micro" #choose which size to use, be aware of costs if going out of free tier
 }
