e083d9695565f403714106755187ce072bfa8fcf
Dave McKinstry <dmckinstry@users.noreply.github.com>
2021-03-06 16:31:14 -0700

    Update main.tf

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -1,4 +1,16 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = "~> 0.14"
+
   backend "remote" {
     organization = "guac_cloud"
 
@@ -7,11 +19,36 @@ terraform {
     }
   }
 }
+
+
 provider "aws" {
-  profile = "default"
   region = "us-west-2"
 }
-resource "aws_instance" "test-sm-ec2" {
-  ami = "ami-b9401b89" #make sure to get the updated ami name for your region
-  instance_type = "t2.micro" #choose which size to use, be aware of costs if going out of free tier
+
+resource "random_pet" "sg" {}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-830c94e3"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+}
+
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.sg.id}-sg"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
 }
